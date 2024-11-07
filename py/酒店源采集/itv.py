9795cc7b697ce6717cf01f875b046eb9ed59a947
231006 <109812774+adminouyang@users.noreply.github.com>
2024-11-07 23:21:56 +0800

    Update itv.py

--- py/酒店源采集/itv.py
+++ py/酒店源采集/itv.py
@@ -214,7 +214,7 @@ for url in urls:
 
 results = set(results)  # 去重得到唯一的URL列表
 results = sorted(results)
-with open("itv.txt", 'w', encoding='utf-8') as file:
+with open("./py/酒店源采集/itv.txt", 'w', encoding='utf-8') as file:
     for result in results:
         file.write(result + "\n")
         print(result)
