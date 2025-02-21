e43f294f63365e3e9349e2400012d88b1e8e23ab
231006 <109812774+adminouyang@users.noreply.github.com>
2025-02-21 18:03:33 +0800

    Update main.py

--- py/优质源/main.py
+++ py/优质源/main.py
@@ -225,3 +225,20 @@ if __name__ == "__main__":
     channels, template_channels = filter_source_urls(template_file)
     updateChannelUrlsM3U(channels, template_channels)
 
+
+    ############################################
+# 打开文档并读取所有行 
+with open('py/优质源/live.txt', 'r', encoding="utf-8") as file:
+ lines = file.readlines()
+# 使用列表来存储唯一的行的顺序 
+ unique_lines = [] 
+ seen_lines = set() 
+# 遍历每一行，如果是新的就加入unique_lines 
+for line in lines:
+ if line not in seen_lines:
+  unique_lines.append(line)
+  seen_lines.add(line)
+# 将唯一的行写入新的文档 
+with open('py/优质源/live.txt', 'w', encoding="utf-8") as file:
+ file.writelines(unique_lines)
+os.remove("zubo1.txt")
