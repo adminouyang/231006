e8a95a83acf6730f38fb107f95352aeb260e9c1d
231006 <109812774+adminouyang@users.noreply.github.com>
2025-02-21 20:07:40 +0800

    Update main.py

--- py/优质源/main.py
+++ py/优质源/main.py
@@ -4,6 +4,7 @@ import logging
 from collections import OrderedDict
 from datetime import datetime
 import config
+import os
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s',
                     handlers=[logging.FileHandler("py/优质源/function.txt", "w", encoding="utf-8"), logging.StreamHandler()])
@@ -221,20 +222,20 @@ def updateChannelUrlsM3U(channels, template_channels):
             
             
             # 打开文档并读取所有行 
-        with open('py/优质源/live1.txt', 'r', encoding="utf-8") as file:
-         lines = file.readlines()
-        # 使用列表来存储唯一的行的顺序 
-         unique_lines = [] 
-         seen_lines = set() 
-        # 遍历每一行，如果是新的就加入unique_lines 
-        for line in lines:
-         if line not in seen_lines:
-          unique_lines.append(line)
-          seen_lines.add(line)
-        # 将唯一的行写入新的文档 
-        with open('py/优质源/live.txt', 'w', encoding="utf-8") as file:
-         file.writelines(unique_lines)
-        os.remove("py/优质源/live1.txt")
+                with open('py/优质源/live1.txt', 'r', encoding="utf-8") as file:
+                 lines = file.readlines()
+                # 使用列表来存储唯一的行的顺序 
+                 unique_lines = [] 
+                 seen_lines = set() 
+                # 遍历每一行，如果是新的就加入unique_lines 
+                for line in lines:
+                 if line not in seen_lines:
+                  unique_lines.append(line)
+                  seen_lines.add(line)
+                # 将唯一的行写入新的文档 
+                with open('py/优质源/live.txt', 'w', encoding="utf-8") as file:
+                 file.writelines(unique_lines)
+                os.remove("py/优质源/live1.txt")
 
 
 if __name__ == "__main__":
