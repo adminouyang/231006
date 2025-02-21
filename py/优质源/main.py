a98d9b4e65e959dde87ac3670b169188dd0e46dc
231006 <109812774+adminouyang@users.noreply.github.com>
2025-02-21 20:32:06 +0800

    Update main.py

--- py/优质源/main.py
+++ py/优质源/main.py
@@ -230,8 +230,8 @@ def updateChannelUrlsM3U(channels, template_channels):
     # 遍历每一行，如果是新的就加入unique_lines 
     for line in lines:
         if line not in seen_lines:
-        unique_lines.append(line)
-        seen_lines.add(line)
+          unique_lines.append(line)
+          seen_lines.add(line)
     # 将唯一的行写入新的文档 
     with open('py/优质源/live.txt', 'w', encoding="utf-8") as file:
         file.writelines(unique_lines)
