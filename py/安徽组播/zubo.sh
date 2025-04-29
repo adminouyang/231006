b76ba7f6102a74c00900f170fa459a4eae001085
231006 <109812774+adminouyang@users.noreply.github.com>
2025-04-29 13:40:03 +0800

    Update zubo.sh

--- py/安徽组播/zubo.sh
+++ py/安徽组播/zubo.sh
@@ -25,15 +25,15 @@ case $city_choice in
         channel_key="安徽电信"
         url_fofa="https://fofa.info/result?qbase64=InVkcHh5IiAmJiByZWdpb249IkFuaHVpIiAmJiBvcmc9IkNoaW5hbmV0IiAmJiBwcm90b2NvbD0iaHR0cCI%3D&page=1&page_size=20"
         ;;
-     2)
-        city="安徽联通"
-        stream="rtp/238.1.79.27:4328"
-        channel_key="安徽联通"
-        url_fofa="https://fofa.info/result?qbase64=InVkcHh5IiAmJiByZWdpb249IkFuaHVpIiAmJiBvcmc9IkNISU5BIFVOSUNPTSBDaGluYTE2OSBCYWNrYm9uZSIgJiYgcHJvdG9jb2w9Imh0dHAi&page=1&page_size=20"
-        ;;       
+     # 2)
+     #    city="安徽联通"
+     #    stream="rtp/238.1.79.27:4328"
+     #    channel_key="安徽联通"
+     #    url_fofa="https://fofa.info/result?qbase64=InVkcHh5IiAmJiByZWdpb249IkFuaHVpIiAmJiBvcmc9IkNISU5BIFVOSUNPTSBDaGluYTE2OSBCYWNrYm9uZSIgJiYgcHJvdG9jb2w9Imh0dHAi&page=1&page_size=20"
+     #    ;;       
     0)
         # 逐个处理{ }内每个选项
-        for option in {1..2}; do
+        for option in {1}; do
           bash "$0" $option  # 假定fofa.sh是当前脚本的文件名，$option将递归调用
         done
         exit 0
