c63a1df0cbb5190e75a84b965ec009dfbf134447
231006 <109812774+adminouyang@users.noreply.github.com>
2025-11-10 23:06:58 +0800

    Update test.py

--- py/测试/test.py
+++ py/测试/test.py
@@ -12,7 +12,7 @@ FOFA_URLS = {
     "https://fofa.info/result?qbase64=InVkcHh5IiAmJiBjb3VudHJ5PSJDTiI%3D": "ip.txt",
 }
 HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
 }
 
 IP_DIR = "py/测试/ip"
