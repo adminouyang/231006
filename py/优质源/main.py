d4dc30c640a68c1a313b8b86cccf2ba51055892f
231006 <109812774+adminouyang@users.noreply.github.com>
2025-11-24 10:53:26 +0800

    Update main.py

--- py/优质源/main.py
+++ py/优质源/main.py
@@ -404,8 +404,11 @@ def finalize_output(organized, group_order, channel_order):
                 urls = sorted(organized[ip_type][group][channel], key=lambda x: x[1], reverse=True)
                 selected = [u[0] for u in urls[:10]]
 
-                if selected:
-                    txt_lines.append(f"{channel},{'#'.join(selected)}")
+                # if selected:
+                #     txt_lines.append(f"{channel},{'#'.join(selected)}")
+                    # 修改这里：每个URL单独一行
+                    for url in selected:
+                        txt_lines.append(f"{channel},{url}")
                     for url in selected:
                         m3u_lines.append(f'#EXTINF:-1 tvg-name="{channel}"tvg-logo="https://gh.catmak.name/https://raw.githubusercontent.com/fanmingming/live/main/tv/{channel}.png" group-title="{group}",{channel}\n{url}')
 
@@ -417,8 +420,11 @@ def finalize_output(organized, group_order, channel_order):
             for channel in extra:
                 urls = sorted(organized[ip_type][group][channel], key=lambda x: x[1], reverse=True)
                 selected = [u[0] for u in urls[:10]]
-                if selected:
-                    txt_lines.append(f"{channel},{'#'.join(selected)}")
+                # if selected:
+                #     txt_lines.append(f"{channel},{'#'.join(selected)}")
+                # 修改这里：每个URL单独一行
+                    for url in selected:
+                        txt_lines.append(f"{channel},{url}")
                     for url in selected:
                         m3u_lines.append(f'#EXTINF:-1 tvg-name="{channel}" group-title="{group}",{channel}\n{url}')
 
