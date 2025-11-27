ce90543c2d0b3d8306a0c3b303030561f894b387
231006 <109812774+adminouyang@users.noreply.github.com>
2025-11-27 22:23:42 +0800

    Update fofa_fetch.py

--- py/fofa/fofa_fetch.py
+++ py/fofa/fofa_fetch.py
@@ -454,7 +454,7 @@ def third_stage():
 
     # 写 IPTV.txt（包含更新时间与分类）
     beijing_now = datetime.now(timezone(timedelta(hours=8))).strftime("%Y-%m-%d %H:%M:%S")
-    disclaimer_url = "https://kakaxi-1.asia/LOGO/Disclaimer.mp4"
+    disclaimer_url = "https://txmov2.a.kwimgs.com/bs3/video-hls/5230086603510673905_hlsb.m3u8?pkey=AAUyMbVkU7WkimubgX0RPM_gxiBDBZ0O6NjXg8fqHAfjVU3xFbWk1Mq_dhogP1fSZfTuxFuTTP4Q9geCGPwtGGOxQWCeEZMzc8A0cmLTyXrZZWUeTZQtSukPK5Y5UyZPBuM"
 
     try:
         with open(IPTV_FILE, "w", encoding="utf-8") as f:
